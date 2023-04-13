63b2477c0017f4ce3d1d96658ee554228081fde6
Muhammad Shoaib <mshoaibdev@gmail.com>
2023-04-13 17:17:53 +0500

    migration

--- database/migrations/2023_04_12_134712_create_chapters_table.php
+++ database/migrations/2023_04_12_134712_create_chapters_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('chapters', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->foreignId('handbook_id')->constrained()->onDelete('cascade');
+            $table->integer('handbook_id');
             $table->timestamps();
         });
     }
